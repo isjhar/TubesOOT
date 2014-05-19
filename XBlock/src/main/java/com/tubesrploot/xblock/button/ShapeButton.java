1ba986dd6b971515bd153a5613f11ba6e4842b5b
farizko <mfarizko@gmail.com>
2014-05-19 15:18:54 +0700

    tambah komen gan

--- XBlock/src/main/java/com/tubesrploot/xblock/button/ShapeButton.java
+++ XBlock/src/main/java/com/tubesrploot/xblock/button/ShapeButton.java
@@ -15,19 +15,19 @@ import javax.swing.JButton;
  * @author Isjhar-pc
  */
 public class ShapeButton extends JButton implements ActionListener{
-    private static final int N_HEIGHT = 40;
-    private static final int N_WIDTH = 40;
+    private static final int N_HEIGHT = 40;//inisiasi dimensi tinggi
+    private static final int N_WIDTH = 40;//inisiasi dimensi lebar
     private ShapeToolBar toolbar;
     private Dimension dimension;
     public ShapeButton(){
         super();
         this.toolbar = new ShapeToolBar();
-        setToolTipText("shape button");
+        setToolTipText("shape button");//menampilkan popup text
         initDimension();
     }
     
      private void initDimension(){
-        dimension = new Dimension(N_WIDTH, N_HEIGHT);
+        dimension = new Dimension(N_WIDTH, N_HEIGHT);//mengatur dimensi objek sesuai parameter input
         setPreferredSize(dimension);
     }
     
