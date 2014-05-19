48b87dd402ae1e49ddcd72db48063e42cccde951
farizko <mfarizko@gmail.com>
2014-05-19 14:31:29 +0700

    tambah komen gan

--- XBlock/src/main/java/com/tubesrploot/xblock/button/CanvasPanel.java
+++ XBlock/src/main/java/com/tubesrploot/xblock/button/CanvasPanel.java
@@ -23,27 +23,27 @@ import org.jhotdraw.draw.DrawingView;
  * @author Isjhar-pc
  */
 public class CanvasPanel extends JScrollPane{
-    private static final int N_HEIGHT = 600;
-    private static final int N_WIDTH = 600;
-    private DrawingView canvas;
-    private DrawingEditor editor;
-    
+    private static final int N_HEIGHT = 600; //menginisiasi besar dimensi tinggi dari canvas
+    private static final int N_WIDTH = 600; //menginisiasi besar dimensi lebar dari canvas
+    private DrawingView canvas; //inisiasi variabel canvas 
+    private DrawingEditor editor; //inisiasi variabel editor
+    //mengatur tampilan dari canvas
     public CanvasPanel(DrawingView canvas){
         super(canvas.getComponent());
         this.canvas = canvas;
-        initDimension();
-        setBorder(BorderFactory.createLineBorder(Color.black));
-        Drawing gambar = new DefaultDrawing();
-        canvas.setDrawing(gambar);
+        initDimension(); //mengatur dimensi canvas
+        setBorder(BorderFactory.createLineBorder(Color.black)); //membuat garis pinggir
+        Drawing gambar = new DefaultDrawing(); //inisiai variabel gambar
+        canvas.setDrawing(gambar); //memasukkan gambar ke dalam canvas
         editor = new DefaultDrawingEditor();
-        editor.add(canvas); 
+        editor.add(canvas); //menambahkan canvas kedalam editor
     }
-    
+    //mengatur dimensi canvas sesuai variabel yang sudah di inisiasi
     private void initDimension(){
         Dimension dimension = new Dimension(N_WIDTH, N_HEIGHT);
-        setPreferredSize(dimension);
+        setPreferredSize(dimension); //mengatur dimensi sesuai hasil inisiasi
     }
-
+    //menampilkan editor
     public DrawingEditor getEditor(){
         return editor;
     }
